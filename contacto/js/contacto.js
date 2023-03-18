78948839d40896e9d076130c53d15493523953ed
letym14 <lety.mtnez02@gmail.com>
2023-03-18 09:09:46 +0100

    .

--- contacto/js/contacto.js
+++ contacto/js/contacto.js
@@ -1,29 +1,10 @@
-/*$(document).ready(function() {
-  $('#contact-form').submit(function(e) {
-    e.preventDefault(); // prevent the form from submitting
-    
-    // get the form data
-    const formData = {
-      name: $('#name').val(),
-      email: $('#email').val(),
-      message: $('#message').val()
-    };
-    
-    // send the form data to the server
-    $.ajax({
-      url: 'send-email.php', // replace with your server-side script
-      method: 'POST',
-      data: formData
-    }).done(function(response) {
-      // handle the response from the server
-      console.log(response);
-      alert('Your message has been sent!');
-      $('#name').val('');
-      $('#email').val('');
-      $('#message').val('');
-    }).fail(function() {
-      // handle any errors
-      alert('An error occurred while sending your message.');
-    });
-  });
-});*/
+$(document).ready(function () {
+  const button = document.querySelector('#button');
+  button.addEventListener('click', () => {
+    const destinatario = 'lety.mtnez02@gmail.com';
+    const subject = document.querySelector('#asunto').value;
+    const message = document.querySelector('#mensaje').value;
+    window.location.href = `mailto:${destinatario}?subject=${subject}&body=${message}`;
+  });
+});
+
