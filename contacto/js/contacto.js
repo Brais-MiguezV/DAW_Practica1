3c7f36ba13ff45c117f12c834329be4729ee8a2b
letym14 <lety.mtnez02@gmail.com>
2023-03-18 14:52:13 +0100

    .

--- contacto/js/contacto.js
+++ contacto/js/contacto.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-  const button = document.querySelector('#button');
-  button.addEventListener('click', () => {
-    const destinatario = 'lety.mtnez02@gmail.com';
-    const subject = document.querySelector('#asunto').value;
-    const message = document.querySelector('#mensaje').value;
-    window.location.href = `mailto:${destinatario}?subject=${subject}&body=${message}`;
-  });
+ 
+  
 });
+
+
+
+
+
+
+
 
