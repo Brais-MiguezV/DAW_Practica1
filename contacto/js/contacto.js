3b7a122e93d40634f8a23520c017d7ff5652b023
letym14 <lety.mtnez02@gmail.com>
2023-03-25 19:32:54 +0100

    ñk

--- contacto/js/contacto.js
+++ contacto/js/contacto.js
@@ -1,27 +1,26 @@
-$("form").submit(function(event) {
-    event.preventDefault(); // Previene que el formulario se envíe por el método por defecto
-  
+/*$("form").submit(function(event) {*/
+function sendEmail() {
     // Recoge los datos del formulario
     var formData = {};
-    $.each($(this).serializeArray(), function(i, field) {
-      formData[field.name] = field.value;
+    $.each($(this).serializeArray(), function (i, field) {
+        formData[field.name] = field.value;
     });
-  
+
     // Envía el correo electrónico con los datos del formulario
     Email.send({
-      Host: "smtp.gmail.com",
-      Username: "tucorreo@gmail.com",
-      Password: "tucontraseña",
-      To: "lety.mtnez02@gmail.com",
-      From: formData.correo,
-      Subject: formData.asunto,
-      Body: "Nombre: " + formData.nombre + "<br>" +
+        Host: "smtp.gmail.com",
+        Username: "tucorreo@gmail.com",
+        Password: "tucontraseña",
+        To: "lety.mtnez02@gmail.com",
+        From: formData.correo,
+        Subject: formData.asunto,
+        Body: "Nombre: " + formData.nombre + "<br>" +
             "Apellidos: " + formData.apellidos + "<br>" +
             "Correo electrónico: " + formData.correo + "<br>" +
             "Teléfono: " + formData.telefono + "<br>" +
             "Mensaje: " + formData.mensaje,
-    }).then(function(message) {
-      alert("El correo electrónico ha sido enviado");
+    }).then(function (message) {
+        alert("El correo electrónico ha sido enviado");
     });
-  });
-  
+}
+
