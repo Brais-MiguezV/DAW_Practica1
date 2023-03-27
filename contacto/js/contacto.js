3f5d2008d7d64fc1e9d9985f929350791e6eb32f
letym14 <lety.mtnez02@gmail.com>
2023-03-27 13:26:14 +0200

    c

--- contacto/js/contacto.js
+++ contacto/js/contacto.js
@@ -1,25 +1,33 @@
-function sendEmail() {
-    // Recoge los datos del formulario
-    var formData = {};
-    $.each($(this).serializeArray(), function (i, field) {
-        formData[field.name] = field.value;
-    });
 
-    // Envía el correo electrónico con los datos del formulario
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "lety.mtnez02@gmail.com",
-        Password: "tucontraseña",
-        To: "lety.mtnez02@gmail.com",
-        From: formData.correo,
-        Subject: formData.asunto,
-        Body: "Nombre: " + formData.nombre + "<br>" +
-            "Apellidos: " + formData.apellidos + "<br>" +
-            "Correo electrónico: " + formData.correo + "<br>" +
-            "Teléfono: " + formData.telefono + "<br>" +
-            "Mensaje: " + formData.mensaje,
-    }).then(function (message) {
-        alert("El correo electrónico ha sido enviado");
-    });
+function sendEmail() {
+    // Obtenemos los valores de los campos del formulario
+    const nombre = document.getElementById('nombre').value;
+    const apellidos = document.getElementById('apellidos').value;
+    const email = document.getElementById('correo').value;
+    const telefono = document.getElementById('telefono').value;
+    const asunto = document.getElementById('asunto').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // significa que debe tener un @ y un punto
+    const telefonoRegex = /^\d{9}$/; // significa que debe tener 9 dígitos
+
+    // Comprobamos que los campos no estén vacíos y que el email y el teléfono sean válidos
+    if (nombre != '' || apellidos != '' || asunto != '' || mensaje != '' || email != '' || telefono != '') {
+        if (emailRegex.test(email) && telefonoRegex.test(telefono)) {
+            alert('¡Mensaje enviado con éxito!');
+        } else if (emailRegex.test(email) && !telefonoRegex.test(telefono)) {
+            alert('Por favor, introduce un teléfono válido.');
+        } else if (!emailRegex.test(email) && telefonoRegex.test(telefono)) {
+            alert('Por favor, introduce un correo electrónico válido.');
+        } else {
+            alert('Por favor, introduce un correo electrónico y un teléfono válidos.');
+        }
+    } else {
+        alert('Por favor, rellena todos los campos.');
+    }
+
 }
 
+// Añadimos el evento click al botón de enviar
+const botonEnviar = document.querySelector('.button');
+botonEnviar.addEventListener('click', sendEmail);
