3fd0c82665e4d2e1f18bff39c0f2a7c626328f26
Brais-MiguezV <braismiguezpintor@gmail.com>
2023-03-29 17:30:46 +0200

    seffsd

--- js/main.js
+++ js/main.js
@@ -51,6 +51,23 @@ document.querySelector(".spanFinal").addEventListener("mouseout", ()=>{
 
 
 
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "../ficheroXML.xml", true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
+}
+
+
+
+
+
 
 
 
